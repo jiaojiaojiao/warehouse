ce30cd1d75ecac76080418e3d791c8db89b2fbf8
zhouxs <zhouxiaoshuang@zbj.com>
2019-01-20 00:59:41 +0800

    ajax

--- src/Material.vue
+++ src/Material.vue
@@ -29,17 +29,10 @@ import companyItem from "./components/CompanyItem.vue";
 import titleLeft from './components/TitleLeft.vue';
 import titleRight from './components/TitleRight.vue';
 
-import axios from './utils/axios'
+import ajax from './utils/ajax'
 
 const pointList = [[1005,487], [-200,426], [-1092,-310], [-285,-860], [880,-547]]
 const headerTitle={monitor: '在途监控', management: '任务配送管理'}
-// let pointList = [
-//   { coordinate: [1005,487] },
-//   { coordinate: [-200,426] },
-//   { coordinate: [-1092,-310] },
-//   { coordinate: [-285,-860] },
-//   { coordinate: [880,-547] }
-// ]
 export default {
   data() {
     return {
@@ -69,36 +62,34 @@ export default {
     const _this = this
     _this.changeWidth()
     window.addEventListener("resize", _this.changeWidth.bind(_this))
-    axios({
-      url: '/API/VehicleMonitor/MonitorQuery.ashx',
-      methods: 'post'
-    })
-    _this.activePoint = [
-    {
-        "trajectoryid": 1,
-        "trajectorycode": "170a58f39b3948199ab02e2986b0b338",
-        "createtime": "2019-01-07 15:02:13.180",
-        "carname": "长安",
-        "licenseplate": "渝A123456",
-        "readerid": "55",
-        "carrfid": "792950535",
-        "backtime": "2019-01-07 15:02:13.190",
-        "hexreaderid": "00 37",
-        "hexcarrfid": "2F 43 77 07"
-    },
-    {
-        "trajectoryid": 2,
-        "trajectorycode": "089ad401a68a45d5bd17cd7d7914601d",
-        "createtime": "2019-01-07 14:57:14.253",
-        "carname": "长安",
-        "licenseplate": "渝A123456",
-        "readerid": "55",
-        "carrfid": "792950535",
-        "backtime": "2019-01-07 14:57:14.254",
-        "hexreaderid": "00 37",
-        "hexcarrfid": "2F 43 77 07"
-    }
-]
+    _this.querydata()
+    window.setInterval(_this.querydata, 10000)
+    // _this.activePoint = [
+    // {
+    //     "trajectoryid": 1,
+    //     "trajectorycode": "170a58f39b3948199ab02e2986b0b338",
+    //     "createtime": "2019-01-07 15:02:13.180",
+    //     "carname": "长安",
+    //     "licenseplate": "渝A123456",
+    //     "readerid": "55",
+    //     "carrfid": "792950535",
+    //     "backtime": "2019-01-07 15:02:13.190",
+    //     "hexreaderid": "00 37",
+    //     "hexcarrfid": "2F 43 77 07"
+    // },
+    // {
+    //     "trajectoryid": 2,
+    //     "trajectorycode": "089ad401a68a45d5bd17cd7d7914601d",
+    //     "createtime": "2019-01-07 14:57:14.253",
+    //     "carname": "长安",
+    //     "licenseplate": "渝A123456",
+    //     "readerid": "55",
+    //     "carrfid": "792950535",
+    //     "backtime": "2019-01-07 14:57:14.254",
+    //     "hexreaderid": "00 37",
+    //     "hexcarrfid": "2F 43 77 07"
+    // }
+// ]
 
   },
   methods: {
@@ -121,6 +112,20 @@ export default {
     },
     isPointActive(idx){
       return this.activePoint.find(item => item.trajectoryid === idx)
+    },
+    querydata(){
+      const _this = this
+      ajax({
+        url: 'http://116.62.30.175:8004/API/VehicleMonitor/MonitorQuery.ashx',
+        method: 'post',
+        data: JSON.stringify({
+          userid: '',
+          username: '',
+          page: ''
+        })
+      }).then(res => {
+        _this.activePoint = res.Entity
+      })
     }
   }
 };
