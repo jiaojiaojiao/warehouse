85d63d16fdd1b5d8aadc7d5209780c7466bcb6a8
zhouxs <zhouxiaoshuang@zbj.com>
2019-03-20 09:28:12 +0800

    bug: map line

--- src/Material.vue
+++ src/Material.vue
@@ -2,8 +2,13 @@
   <layout>
     <menu-item slot="header" :title-list="headerTitle"></menu-item>
     <material-item slot="left-bar" :materialData="allPoint"></material-item>
-    <div slot="container" class="map" ref='mapWrap'>
-      <img src="./assets/map.jpg" alt="" :style="`width: ${imgWidth}px; height: ${imgHeight}px`">
+    <div slot="container" class="map" ref="mapWrap">
+      <img
+        class="map-bg"
+        src="./assets/map.jpg"
+        alt
+        :style="`width: ${imgWidth}px; height: ${imgHeight}px`"
+      >
       <div class="anchor">
         <div
           class="anchor-cont"
@@ -11,10 +16,24 @@
           :key="idx"
           :style="calStyle(item)"
         >
-          <div class="tip" v-show="isPointActive(idx)">
-            {{isPointActive(idx) && activePoint.licenseplate}}
-          </div>
+          <div
+            class="tip"
+            v-show="isPointActive(idx)"
+          >{{isPointActive(idx) && activePoint.licenseplate}}</div>
           <div :class="['pulse', isPointActive(idx) ? 'active' : '']"></div>
+        </div>
+        <div
+            class="anchor-line"
+            v-for="(item, idx) in calPointList"
+            :key="idx+'line'"
+            :style="calStyle(item)"
+          >
+          <img
+            v-if="idx >= 1"
+            :ref="`line${idx}`"
+            :src="require(`./assets/map/line${idx + 1}.png`)"
+            alt="l"
+          >
         </div>
       </div>
     </div>
@@ -25,16 +44,22 @@
 import menuItem from "./components/MenuItem.vue";
 import materialItem from "./components/MaterialItem.vue";
 import companyItem from "./components/CompanyItem.vue";
-import titleLeft from './components/TitleLeft.vue';
-import titleRight from './components/TitleRight.vue';
+import titleLeft from "./components/TitleLeft.vue";
+import titleRight from "./components/TitleRight.vue";
 
-import ajax from './utils/ajax'
+import ajax from "./utils/ajax";
 
-const pointList = [[579,660], [593,194], [601,-489], [-287,180], [-677,-687]]
-const headerTitle={monitor: '在途监控', management: '任务配送管理'}
-const defaultW = 3000
-const defaultH = 1885
-const defaultProp = defaultW / defaultH
+const pointList = [
+  [579, 660],
+  [593, 194],
+  [601, -489],
+  [-287, 180],
+  [-677, -687]
+];
+const headerTitle = { monitor: "在途监控", management: "任务配送管理" };
+const defaultW = 3000;
+const defaultH = 1885;
+const defaultProp = defaultW / defaultH;
 export default {
   data() {
     return {
@@ -46,8 +71,8 @@ export default {
       headerTitle,
       queryInterval: null,
       linkObj: {
-        link: '#/storage',
-        title: '出库物资一览'
+        link: "#/storage",
+        title: "出库物资一览"
       }
     };
   },
@@ -59,115 +84,169 @@ export default {
     titleLeft
   },
   computed: {
-    calPointList: function(){
+    calPointList: function() {
       return pointList.map(item => {
-        const { imgprop } = this
-        return [item[0]/imgprop, item[1]/imgprop]
-      })
+        const { imgprop } = this;
+        return [item[0] / imgprop, item[1] / imgprop];
+      });
     }
   },
   mounted() {
-    const _this = this
-    _this.changeWidth()
-    window.addEventListener("resize", _this.changeWidth.bind(_this))
-    _this.querydata()
+    const _this = this;
+    _this.changeWidth();
+    window.addEventListener("resize", _this.changeWidth.bind(_this));
+    _this.querydata();
+
+    // dom ready
+    _this.changeImageWidth();
+    // console.warn($refs)
+  },
+  updated() {
+    this.changeImageWidth();
   },
   methods: {
+    changeImageWidth() {
+      const $refs = this.$refs;
+      Object.keys($refs).map(key => {
+        if (key.startsWith("line")) {
+          if (!this[`${key}_width`]) {
+            this[`${key}_width`] = this.$refs[key][0].width;
+          }
+          const width = this[`${key}_width`];
+
+          this.$refs[key][0].style.visibility = "visible";
+          if (width) {
+            console.warn(width, this.imgprop);
+            this.$refs[key][0].width = width / this.imgprop;
+          }
+        }
+      });
+    },
     changeWidth() {
-      const wrapWidth = this.$refs.mapWrap.clientWidth
-      const wrapHeight = this.$refs.mapWrap.clientHeight
-      if (wrapWidth > wrapHeight * defaultProp){
-        this.imgWidth = wrapHeight * defaultProp
-        this.imgHeight = wrapHeight
-        this.imgprop = defaultH / wrapHeight
+      const wrapWidth = this.$refs.mapWrap.clientWidth;
+      const wrapHeight = this.$refs.mapWrap.clientHeight;
+      if (wrapWidth > wrapHeight * defaultProp) {
+        this.imgWidth = wrapHeight * defaultProp;
+        this.imgHeight = wrapHeight;
+        this.imgprop = defaultH / wrapHeight;
       } else {
-        this.imgWidth = wrapWidth
-        this.imgHeight = wrapWidth / defaultProp
-        this.imgprop = defaultW / wrapWidth
+        this.imgWidth = wrapWidth;
+        this.imgHeight = wrapWidth / defaultProp;
+        this.imgprop = defaultW / wrapWidth;
+      }
+      this.changeImageWidth();
+    },
+    calStyle(item) {
+      const coordinate = item;
+      return `transform: translateX(${coordinate[0]}px) translateY(${
+        coordinate[1]
+      }px)`;
+    },
+    calLineWidth(idx) {
+      const $refs = this.$refs;
+      if (Object.keys($refs).length && this.$refs[`line${idx}`]) {
+        const width = this.$refs[`line${idx}`][0].clientWidth;
+        console.warn(width, `line${idx}`, this.$refs[`line${idx}`][0]);
+        if (width) {
+          return `translate: scale(${width / this.imgprop})`;
+        } else {
+          return `translate: scale(1)`;
+        }
+        // if (this.$refs[`line${idx + 1}`]) {
+        //   const width = this.$refs[`line${idx + 1}`][0].clientWidth;
+        //   console.log(width / this.imgprop);
+        //   if (width) {
+        //     return `width: ${width / this.imgprop}`;
+        //   }
+        // }
       }
     },
-    calStyle(item){
-      const coordinate = item
-      return `transform: translateX(${coordinate[0]}px) translateY(${coordinate[1]}px)`
+    isPointActive(idx) {
+      return (
+        this.activePoint.TrajectoryList &&
+        this.activePoint.TrajectoryList[0].readerid.includes(idx + 1)
+      );
     },
-    isPointActive(idx){
-      return this.activePoint.TrajectoryList && this.activePoint.TrajectoryList[0].readerid.includes(idx+1)
-    },
-    querydata(){
-      const _this = this
+    querydata() {
+      const _this = this;
       ajax({
-        url: 'API/VehicleMonitor/MonitorQuery.ashx',
-        method: 'post',
+        url: "API/VehicleMonitor/MonitorQuery.ashx",
+        method: "post",
         data: JSON.stringify({
-          userid: '',
-          username: '',
-          page: ''
+          userid: "",
+          username: "",
+          page: ""
         })
       }).then(res => {
-          const length = res.Entity.length
-          var i = 0
-          const setMaterial = () => {
-            if(i < length){
-              _this.allPoint = res.Entity
-              _this.activePoint = res.Entity[i]
-              // const trajectoryList = res.Entity[i].TrajectoryList
-              // _this.activePoint=trajectoryList[0]
-              setTimeout(setMaterial, 5000)
-              i++
-            }
-            else {
-              //循环后重新查询
-              _this.querydata();
-            }
+        const length = res.Entity.length;
+        var i = 0;
+        const setMaterial = () => {
+          if (i < length) {
+            _this.allPoint = res.Entity;
+            _this.activePoint = res.Entity[i];
+            // const trajectoryList = res.Entity[i].TrajectoryList
+            // _this.activePoint=trajectoryList[0]
+            setTimeout(setMaterial, 5000);
+            i++;
+          } else {
+            //循环后重新查询
+            _this.querydata();
           }
-          setMaterial();
+        };
+        setMaterial();
         // _this.activePoint = res.Entity
-      })
+      });
     }
   },
   destroyed() {
-    clearInterval(this.queryInterval)
+    clearInterval(this.queryInterval);
   }
 };
 </script>
 <style scoped>
-  @import './assets/styles/animate.css';
-  .map{
-    position: relative;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-  }
-  .map img{
-    position: absolute;
-    max-width: 100%;
-    max-height: 100%;
-    width: auto;
-    height: auto;
-    z-index: 0;
-  }
-  .anchor{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 1px;
-    height: 1px;
-    z-index: 99;
-  }
-  .anchor-cont{
-    position: relative;
-  }
-  .tip{
-    position: absolute;
-    top: -2rem;
-    left: 2rem;
-    width: 12rem;
-    height: 3.6rem;
-    line-height: 3.6rem;
-    font-size: 2.2rem;
-    color: #ffffff;
-    background-color: rgba(88,246, 240, .5);
-  }
+@import "./assets/styles/animate.css";
+.map {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+}
+.map .map-bg {
+  position: absolute;
+  max-width: 100%;
+  max-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: 0;
+}
+.anchor {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 1px;
+  height: 1px;
+  z-index: 99;
+}
+.anchor-cont {
+  position: relative;
+}
+.anchor-line {
+  position: relative;
+}
+.anchor-line img {
+  visibility: hidden;
+}
+.tip {
+  position: absolute;
+  top: -2rem;
+  left: 2rem;
+  width: 12rem;
+  height: 3.6rem;
+  line-height: 3.6rem;
+  font-size: 2.2rem;
+  color: #ffffff;
+  background-color: rgba(88, 246, 240, 0.5);
+}
 </style>
