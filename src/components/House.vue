07417983f5e72c23f1c8ecbd04b4009022827941
丁昭祥 <zhaoxiang.ding@cisdi.com.cn>
2019-05-21 22:30:25 +0800

    add line

--- src/components/House.vue
+++ src/components/House.vue
@@ -5,7 +5,8 @@
       <ul class="houseItems clear">
         <li v-for="item  in list" class="houseItem" :class="item.style">
           <ul class="box">
-            <li class="boxItem" v-for="c in item.column" :class="c.style">
+            <li class="boxLine">{{item.line}}</li>
+            <li class="boxItem" v-if="item.line!='I'" v-for="c in item.column" :class="c.style">
               <!--<a  href="javascript:void(0);" v-for="material in list">
                 <template v-if="material.detailno==item.line && material.detailrow ==c.number">
                   <img src="../assets/boxActive.png" alt="">
@@ -15,7 +16,7 @@
                 </template>
             </a>
            -->
-              <a href="javascript:void(0);" v-if="c.isactive"
+              <a href="javascript:void(0);" v-if="c.isactive "
                  v-tooltip.notrigger="{ content: c.shelfname+'货架'+c.detailrow+'层'+c.detailcolumn+'列'+c.detailno+'号', visible: c.isactive}">
                 <img
                   v-if="c.detailrow==1&&c.detailno==1&&(c.shelfname=='A'||c.shelfname=='B'||c.shelfname=='C'||c.shelfname=='D'||c.shelfname=='E')"
@@ -99,7 +100,6 @@
                 <img v-if="c.detailrow==3&&c.detailno==4&&(c.shelfname=='F'||c.shelfname=='G'||c.shelfname=='H')"
                      src="../assets/box/three/box3-4.gif" alt="">
               </a>
-
               <a href="javascript:void(0);" :title="item.line"
                  v-else-if="item.line=='A'||item.line=='B'||item.line=='C'||item.line=='D'||item.line=='E'">
                 <img src="../assets/box/four.png" alt="">
@@ -107,7 +107,9 @@
               <a href="javascript:void(0);" :title="item.line" v-else="item.line=='F'||item.line=='G'||item.line=='H'">
                 <img src="../assets/box/three.png" alt="">
               </a>
-
+            </li>
+            <li v-else="item.line=='I'" class="boxBigItem">
+                <span class="bigItem"></span>
             </li>
           </ul>
         </li>
@@ -1077,6 +1079,20 @@
               }
             ],
             line: 'H'
+          },
+          {
+            column: [
+              {
+                number: 1,
+                style: 'boxBigItem',
+                isactive: false,
+                shelfname: '',
+                detailrow: '',
+                detailcolumn: '',
+                detailno: ''
+              }
+            ],
+            line: 'I'
           }
         ];
         var materias = this.material;
@@ -1092,6 +1108,7 @@
               }
           });
         });
+
         this.houseList=houses;
         return this.houseList
       }
@@ -1134,7 +1151,7 @@
   }
 
   .houseItems {
-    padding-left: 10rem;
+    padding-left: 1rem;
     padding-bottom: 10rem;
     padding-top: 0.5rem;
     display: inline-block;
@@ -1149,9 +1166,12 @@
   }*/
   .box {
     display: inline-block;
-    transform: rotate(-15deg);
+    transform: rotate(-25deg);
   }
-
+.boxLine{
+  color: white;
+  margin-left: 10px;
+}
   .boxItem img {
     width: 7rem;
     height: 8rem;
@@ -1170,7 +1190,21 @@
     /* margin-right: 1rem;
      margin-bottom: -3rem;*/
   }
-
+.boxBigItem{
+  top:9rem;
+  right:-2.5rem;
+  position: absolute;
+}
+.bigItem {
+  display: block;
+  width: 19rem;
+  height: 7rem;
+  background: red;
+  transform: rotate(-33deg);
+  border: 1px white solid;
+  background: #3d6d96;
+  opacity: 0.5;
+}
   .boxItem2 {
     margin-left: -5.2rem;
     margin-top: -6.5rem;
